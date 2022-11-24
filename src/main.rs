7555d92f544f1434906db80ab995c4c4b1f6d952
exo-cortex <lukipuki@physik.tu-berlin.de>
2022-11-24 16:01:21 +0100

    [update] Update the example in main.rs

--- src/main.rs
+++ src/main.rs
@@ -12,13 +12,18 @@ fn main() {
 
     // my_shelf.add_compartments(&[320.0]);
     my_shelf
-        .add_level(350.0)
-        .add_compartments(&[100.0], Depth::Extra)
-        .add_compartments(&[300.0], Depth::Normal)
+        .add_level(250.0)
+        .add_compartments(&[350.0], Depth::Extra)
+        .add_compartments(&[300.0, 250.0, 150.0, 350.0, 400.0, 250.0], Depth::Normal)
         .extend_to_full_width(Depth::Extra);
     my_shelf
         .add_level(120.0)
         .add_compartments(&[110.0, 400.0, 250.0], Depth::Normal)
+        .extend_to_full_width(Depth::Normal);
+    my_shelf
+        .add_level(350.0)
+        .add_compartments(&[350.0], Depth::Normal)
+        .add_compartments(&[150.0, 350.0, 550.0], Depth::Extra)
         .extend_to_full_width(Depth::Normal);
     my_shelf
         .add_level(350.0)
